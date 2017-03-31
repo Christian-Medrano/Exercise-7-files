431447093c37f63fe9ff24bdfecc5a389b083993
Christian Medrano <christianmikhael@yahoo.com>
2017-03-31 16:46:06 -0700

    html

--- index.php
+++ index.php
@@ -9,4 +9,28 @@
   $treat = 'cookie';
  ?>
 
- 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <title><?php echo $title; ?></title>
+   <link rel="stylesheet" href="css/bootstrap.css">
+ </head>
+ <body>
+  <main class="container py-4">
+   <h1>Meet <?php echo $name; ?>.</h1>
+   <p><?php echo $name ?> is <?php echo $age ?> years old.</p>
+   <p><?php echo $name; ?> likes coding, but his real passion is math.</p>
+   <p>For example, he knows that <?php echo $number1; ?> + <?php echo $number2; ?> = <?php echo $sum; ?>.</p>
+   <p>Good job, Christian. You get a <?php echo $treat; ?>.</p>
+   <p><?php echo $name; ?> also knows that <?php echo ''.$sum.' - '.$number2.' = '.$wrongDif.'.'; ?></p>
+   <p>What? No, <?php echo $name; ?>. That's wrong.</p>
+   <p>Wait, what are you doing with that crowbar?</p>
+   <p><?php echo $name; ?></p>
+   <p><?php echo strtoupper($name); ?>!?!</p>
+   <p>-</p>
+
+
+  </main>
+ </body>
+ </html>
